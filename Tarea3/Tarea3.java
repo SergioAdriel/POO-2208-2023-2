3f39d502b39efd412056fd4e225d0a32d95b9dca
Sergio Adriel Muñoz Camarena <125773032+SergioAdriel@users.noreply.github.com>
2023-02-22 12:31:58 -0600

    Add files via upload

---
+++ Tarea3/Tarea3.java
@@ -0,0 +1,15 @@
+public class Tarea3
+{
+	public static void main(String[] args) {
+	  //Guardamos las variables que vamos a repetir
+	    String vertical = "|   |";
+	    String horizontal = "-----"; 
+	  //Printeamos en orden para poder formar la figura solicitada
+		System.out.println(horizontal);
+		System.out.println(vertical);
+		System.out.println(vertical);
+		System.out.println(vertical);
+		System.out.println(horizontal);
+
+	}
+}
