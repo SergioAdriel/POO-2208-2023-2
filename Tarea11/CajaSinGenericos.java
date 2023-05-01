a48bf33a62723622a4f42006cef78398b900dd9f
Sergio Adriel Muñoz Camarena <125773032+SergioAdriel@users.noreply.github.com>
2023-05-01 12:40:29 -0600

    Add files via upload

---
+++ Tarea11/CajaSinGenericos.java
@@ -0,0 +1,26 @@
+package org.example.generics;
+import java.util.Arrays;
+
+public class CajaSinGenericos {
+    Object[] content;
+
+    public CajaSinGenericos() {
+    }
+
+    @Override
+    public String toString() {
+        return "CajaSinGenericos{" +
+                "content=" + Arrays.toString(content) +
+                '}';
+    }
+
+    public CajaSinGenericos(Object[] content) {
+        this.content = content;
+    }
+    public void add(int pos , Object objeto){
+        content[pos] = objeto;
+    }
+    public Object get(int pos){
+        return content[pos];
+    }
+}
