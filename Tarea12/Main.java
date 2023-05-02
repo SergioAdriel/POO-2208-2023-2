fe690e036063b25cc982ad0e49bc72c9af7f241e
Sergio Adriel Muñoz Camarena <125773032+SergioAdriel@users.noreply.github.com>
2023-05-01 18:37:47 -0600

    Add files via upload

---
+++ Tarea12/Main.java
@@ -0,0 +1,10 @@
+package Tarea12;
+
+import Tarea12.iu.Principal;
+
+public class Main {
+    public static void main(String[] args) {
+        Principal ventana = new Principal("Hola mundo AWT");
+
+    }
+}
---
+++ Tarea12/iu/eventos/EventosRaton.java
@@ -0,0 +1,31 @@
+package Tarea12.iu.eventos;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class EventosRaton implements MouseListener {
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        System.out.printf("se dio clic");
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+}
